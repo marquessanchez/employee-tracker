46baff46c6c27d927830794e271b6f7105d65396
mrican217@gmail.com <mrican217@gmail.com>
2022-04-02 16:48:31 -0400

    routes finished and command line prompting

--- routes/apiRoutes/employeesRoutes.js
+++ routes/apiRoutes/employeesRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cTable = require("console.table");
 const router = express.Router();
+
 const db = require('../../db/connection');
 
 router.put('/api/employees/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const express = require ('express');
-const db = require('./db/connection.js')
+const db = require('./db/connection.js');
+// const cTable = require("c.table");
+const { connect } = require('./db/connection.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,128 +16,128 @@ app.use(express.json());
 
 // Default response for any other request (Not Found)
 // Catchall route, this route can override all other routes
-app.use((req, res) => {
-  res.status(404).end();
-});
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
+// // Start server after DB connection
+// db.connect(err => {
+//   if (err) throw err;
+//   console.log('Database connected.');
   
-  app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-  });
-});
+//   app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//   });
+// });
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3001,
-//     user: 'root',
-//     password: 'password',
-//     database: 'employee'
-// })
+// // const connection = mysql.createConnection({
+// //     host: 'localhost',
+// //     port: 3001,
+// //     user: 'root',
+// //     password: 'password',
+// //     database: 'employee'
+// // })
 
-// connection.connect(function (err) {
-//     if (err) throw err;
+// // connection.connect(function (err) {
+// //     if (err) throw err;
+// //     init();
+// // })
+
+// const init = async () => {
+//     try {
+//         let answer = await inquirer.prompt({
+//             name: 'choice',
+//             type: 'list',
+//             message: 'What would you like to do?',
+//             choices: [
+//                 'View Employees',
+//                 'View Departments',
+//                 'View Roles',
+//                 'Add Employees',
+//                 'Add Departments',
+//                 'Add Roles',
+//                 'Update Employee Role',
+//                 'Exit'
+//             ]
+//         });
+//         switch (answer.choice) {
+//             case 'View Employees':
+//                 employeeView();
+//                 break
+
+//             case 'View Departments':
+//                 departmentView();
+//                 break
+
+//             case 'View Roles':
+//                 roleView();
+//                 break
+
+//             case 'Add Employees':
+//                 employeeAdd();
+//                 break
+
+//             case 'Add Departments':
+//                 departmentAdd();
+//                 break
+
+//             case 'Add Roles':
+//                 roleAdd();
+//                 break
+
+//             case 'Update Employee Role':
+//                 employeeUpdate();
+//                 break
+
+//             case 'Exit':
+//                 connection.end();
+//                 break
+//         };
+//     } catch (err) {
+//         console.log(err);
+//         init();
+//     };
+// }
+
+// const employeeView = async () => {
+//     // db.connect.promise().query('select * from employees;').then(data=> console.log(data));
+//     db.promise().query(`
+//         SELECT
+//             e.id,
+//             e.first_name,
+//             e.last_name, 
+//             r.title,
+//             d.name,
+//             r.salary,
+//             CONCAT(e2.first_name+' '+e2.last_name)
+//         FROM employees e
+//         JOIN roles r
+//         ON e.role_id=r.id
+//         JOIN department d
+//         ON r.department_id=d.id
+//         JOIN employees e2
+//         ON e.manager_id=e2.id;`
+//     ).then((rows)=> console.log(rows))
+//     // init();
+// }
+
+// const departmentView = async () => {
 //     init();
-// })
+// }
 
-const init = async () => {
-    try {
-        let answer = await inquirer.prompt({
-            name: 'choice',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: [
-                'View Employees',
-                'View Departments',
-                'View Roles',
-                'Add Employees',
-                'Add Departments',
-                'Add Roles',
-                'Update Employee Role',
-                'Exit'
-            ]
-        });
-        switch (answer.choice) {
-            case 'View Employees':
-                employeeView();
-                break
+// const roleView = async () => {
+//   init();
+// }
 
-            case 'View Departments':
-                departmentView();
-                break
-
-            case 'View Roles':
-                roleView();
-                break
-
-            case 'Add Employees':
-                employeeAdd();
-                break
-
-            case 'Add Departments':
-                departmentAdd();
-                break
-
-            case 'Add Roles':
-                roleAdd();
-                break
-
-            case 'Update Employee Role':
-                employeeUpdate();
-                break
-
-            case 'Exit':
-                connection.end();
-                break
-        };
-    } catch (err) {
-        console.log(err);
-        init();
-    };
-}
-
-const employeeView = async () => {
-    // db.connect.promise().query('select * from employees;').then(data=> console.log(data));
-    db.promise().query(`
-        SELECT
-            e.id,
-            e.first_name,
-            e.last_name, 
-            r.title,
-            d.name,
-            r.salary,
-            CONCAT(e2.first_name+' '+e2.last_name)
-        FROM employees e
-        JOIN roles r
-        ON e.role_id=r.id
-        JOIN department d
-        ON r.department_id=d.id
-        JOIN employees e2
-        ON e.manager_id=e2.id;`
-    ).then((rows)=> console.log(rows))
-    // init();
-}
-
-const departmentView = async () => {
-    init();
-}
-
-const roleView = async () => {
-  init();
-}
-
-employeeView();
+// employeeView();
 
 // init();
 
 // Create a function to write README file
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(),fileName),data);
-}
-
+// function writeToFile(fileName, data) {
+//   return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+// }
+// 
 // Create a function to initialize app
 // function init() {
 //   inquirer.prompt(questions).then(res=>{
@@ -176,71 +178,119 @@ function writeToFile(fileName, data) {
 //   });
 // });
 
-function toDo() {
-        let answer = await inquirer.prompt({
-            name: 'choice',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: [
-                'View Employees',
-                'View Departments',
-                'View Roles',
-                'Add Employees',
-                'Add Departments',
-                'Add Roles',
-                'Update Employee Role',
-                'Exit'
-            ]
-        });
-        switch (answer.choice) {
-            case 'View Employees':
-                employeeView();
-                break
+const init = () => {
+    let answer = inquirer.prompt({
+        name: 'choice',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View Employees',
+            'View Departments',
+            'View Roles',
+            'Add Employees',
+            'Add Departments',
+            'Add Roles',
+            'Update Employee Role',
+            'Exit'
+        ]
+    });
+    switch (answer.choice) {
+        case 'View Employees':
+             viewEmployee();
+             break;
 
-            case 'View Departments':
-                departmentView();
-                break
+        case 'View Departments':
+            viewDepartment();
+            break;
 
-            case 'View Roles':
-                roleView();
-                break
+        case 'View Roles':
+            viewRole();
+             break;
 
-            case 'Add Employees':
-                employeeAdd();
-                break
+        case 'Add Employees':
+            addEmployee();
+            break;
 
-            case 'Add Departments':
-                departmentAdd();
-                break
+        case 'Add Departments':
+            addDepartment();
+            break;
 
-            case 'Add Roles':
-                roleAdd();
-                break
+        case 'Add Roles':
+            addRole();
+            break;
 
-            case 'Update Employee Role':
-                employeeUpdate();
-                break
+        case 'Update Employee Role':
+            updateEmployee();
+            break;
 
-            case 'Exit':
-                connection.end();
-                break
-        };
+        case 'Exit':
+            connection.end();
+            break;
+    };
    
 }
 
 // add case functions here
-const employeeView = async () => {
-    
-
-    init();
+const addDepartment = async () => {
+    inquirer.prompt({
+        type: "input",
+        message: "What is the department name?",
+        name: "department"
+    })
+    .answer = () => {
+        connect.query("INSERT INTO department (department) VALUES (?)", [answer.department], err => {
+            if (err) throw err;
+            console.table(res);
+            init();
+        })
+    }
+}
+const addRole = async () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is their title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is their department id?", 
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is their salary?",
+            name: "salary"
+        }
+    ])
+    .answer = () => {
+        connect.query("INSERT INTO role (title, department, salary)) VALUES (?,?,?)", [answer.title, answer.department, answer.salary], err => {
+            if (err) throw err;
+            console.table(res);
+            init();
+        })
+    }
 }
 
-const departmentView = async () => {
-    
+const viewDepartment = async () => {
+    let query = "SELECT * FROM department";
+    connect.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
 }
 
-const roleView = async () => {
-    
+const viewRole = async () => {
+    let query = "SELECT * FROM role";
+    connect.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
+}
+const exit = async () => {
+    connect.end();
 }
 
 init();
